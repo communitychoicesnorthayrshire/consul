8b39273a6d393ffe01c2a4cc9ea59dfad218030d
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2019-04-25 14:08:15 +0100

    Amend terminology as requested by NAY Council

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205131722) do
+ActiveRecord::Schema.define(version: 20190327094436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1579,6 +1579,12 @@ ActiveRecord::Schema.define(version: 20190205131722) do
     t.integer  "limit",      default: 3
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "zipcodes", force: :cascade do |t|
+    t.string   "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "administrators", "users"
