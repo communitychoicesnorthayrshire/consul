510af09027cec47f97c4cd618fae2dff7b6fc62d
Tom Clive <tomclive@Jamess-MacBook-Air.local>
2019-08-29 13:46:32 +0100

    Add additional user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 # Default admin user (change password after first deploy to a server!)
-if Administrator.count == 1 && !Rails.env.test?
+if Administrator.count == 0 && !Rails.env.test?
   admin = User.create!(username: "admin2", email: "consul@cosla.gov.uk", password: "12345678",
                        password_confirmation: "12345678", confirmed_at: Time.current,
                        terms_of_service: "1")
